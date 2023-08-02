c18a55f9382ca2d76358ce5702f0e782b87812b5
Przemek <krepski80@gmail.com>
2023-08-02 21:10:48 +0200

    storing and managing  datas

--- tic tac toe/scripts/app.js
+++ tic tac toe/scripts/app.js
@@ -1,13 +1,19 @@
-let editedPlayer = 0;
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
 
-const players=[
+let editedPlayer = 0;
+let activePlayer = 0;
+const players = [
     {
-        name:'',
-        symbol:'X'
+        name: '',
+        symbol: 'X'
     },
     {
-        name:'',
-        symbol:'O'
+        name: '',
+        symbol: 'O'
     }
 ];
 
@@ -15,11 +21,15 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
-
+const startNewGameBtnElement = document.getElementById('start-game-btn');
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board')
 
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
@@ -29,3 +39,13 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+s
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// for( const gameFieldElement of gameFieldElements){
+//
+//     gameFieldElement.addEventListener('click', selectGameField);
+// }
+
+gameBoardElement.addEventListener('click', selectGameField);
