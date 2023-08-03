3424d52a6a5119690b9f79449bb58666cdddc25c
Przemek <krepski80@gmail.com>
2023-08-03 21:08:16 +0200

    express frist code

--- node/appie.js
+++ node/appie.js
@@ -1,1 +1,16 @@
-const username = 'Przemek';
+const express = require('express');
+
+
+const app = express();
+
+app.get('/currenttime', function(req,resp){
+resp.send('<h2>' + new Date().toISOString() + '</h2>');
+});
+app.get('/', function(req,resp){
+resp.send('<h1>Siema nigga</h1>');
+});
+
+app.listen(3000);
+
+
+
