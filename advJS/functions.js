8ff6549b953b2e8f3992876d01ca3898acc7ab6b
Przemek <krepski80@gmail.com>
2023-08-07 07:25:56 +0200

    advanced part finished

--- advJS/functions.js
+++ advJS/functions.js
@@ -1,5 +1,19 @@
-function greet(){
+function greet() {
     console.log("siemeczka");
 }
 
 greet();
+
+
+function sumUp(...numbers) {
+    let result = 0;
+    for (const number of numbers) {
+        result += number;
+    }
+    return result;
+}
+const inputValues = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(sumUp(...inputValues))
+
+console.log(sumUp)
